1f0ce3e013f07ca203adc8ee4d18b68a1be2f87f
BDel121 <104743857+BDel121@users.noreply.github.com>
2022-07-08 21:21:20 -0500

    Update tradebotalgo.py

--- tradebotalgo.py
+++ tradebotalgo.py
@@ -6,8 +6,11 @@ import plotly.express as px
 import numpy as np
 import time
 
-# The api key DEMO you must sign up for a free demo key at https://site.financialmodelingprep.com/developer/docs/dashboard/
-api_key = 'DEMO'
+# create a True phrase or a phrase that enables and disables a while loop
+do = True
+
+# The api key is "494a1352ffaaa042a40101d33af58da0"
+api_key = '494a1352ffaaa042a40101d33af58da0'
 def simplefile():
     # simple code to write a json file so you can load the information into it and if its there then to pass 
     try:
@@ -19,13 +22,16 @@ def simplefile():
 def checkfile():
     with open('StockDataInfo.json','r') as StockDataR:
         try:
+        #   try is used to try to see if something works and if there is an error then it will move on to the except
             StockData = json.load(StockDataR)
             if StockData != []:
+            #   if data already populates the json file 
                 
                 append_to_file()
                 stock_research = requests.get(f'https://financialmodelingprep.com/api/v3/available-traded/list?apikey={api_key}')
                 stock_research = stock_research.json()
             
+                #print lengths to make sure all data is aquired
                 listname1 = []
                 for f in stock_research:
                      stock_symbol = f['symbol']
@@ -39,20 +45,20 @@ def checkfile():
                     listname2.append(name)
                 print(len(listname2))
 
-                if len(listname1) == len(listname2):
-                    data_print()
-                    return False
-
-                
-                
+                #if len(listname1) == len(listname2):
+                    #if the two data sets are the same length then all the data is aquired
+                data_print()
+                return False
 
             elif StockData == []:
+            # if data is empty then go to writing for the file
                 write_to_file()
         except:
             write_to_file()
             pass
 
 def append_json_file(new_data, StockFile="StockDataInfo.json"):
+    #   write data to the end of the json file in an easy and repeatable way
     with open(StockFile,'r+') as Sfile:
         sfile_data = json.load(Sfile)
         sfile_data.append(new_data)
@@ -69,6 +75,7 @@ def write_to_file():
         stock_research = stock_research.json()
     
         allstockinfo = {}
+        #   create an initial dictionary for information
         for StockR in stock_research:
             stock_symbol = StockR['symbol']
             stock_name = StockR['name']
@@ -113,7 +120,9 @@ def write_to_file():
                 StockNet = StockI[0]['netIncome']
                 Sdate         = StockI[0]["date"]
 
-            allstockinfo[stock_symbol] = {}
+            #   create a dict within
+            allstockinfo[stock_symbol] = {} 
+            #   create a nested information within a dict
             allstockinfo[stock_symbol]['symbol']        = stock_symbol
             allstockinfo[stock_symbol]['name']          = stock_name
             allstockinfo[stock_symbol]['industry']      = stock_industry
@@ -127,11 +136,10 @@ def write_to_file():
             allstockinfo[stock_symbol]['netincome']     = StockNet
 
             #print(stock_symbol,stock_name,stock_price,'\n',stock_industry,stock_sector,stock_country,'\n',StockValue,StockDebt,SVal,'\n',SVal1)
-            
             #with plan 0.2 * number of calls
-            # 0.2* 4 =0.8 = 1sec
+            # 0.2* 4 =0.8 = rounded to 1sec
             # 1 for testing with a data plan and 1728 for full work with a demo plan
-            time.sleep(1728)
+            time.sleep(1)
             #print(stock_symbol)
             append_json_file(allstockinfo[stock_symbol])
     except:
@@ -157,13 +165,14 @@ def append_to_file():
         thislist1 = []
         # create a list
         for sname in StockData:
-            name1 = sname['symbol']
+            name1 = sname['name']
             thislist1.append(name1)
             #sort through the data in the json file and add it to the list
 
         total_stock_list = []
 
         allstockinfo = {}
+        #   create an initial dictionary for information
         for StockR in stock_research:
             stock_symbol = StockR['symbol']
             stock_name = StockR['name']
@@ -171,7 +180,7 @@ def append_to_file():
 
             total_stock_list.append(stock_symbol)
 
-            if stock_symbol not in thislist1:
+            if stock_name not in thislist1:
                 # check to see if a stock is already in the list or not if it is then pass and if it isnt then run program to add it to the json file 
             
                 StockP = requests.get(f'https://financialmodelingprep.com/api/v3/profile/{stock_symbol}?apikey={api_key}').json()
@@ -212,11 +221,11 @@ def append_to_file():
                     StockCostRev = StockI[0]['costOfRevenue']
                     StockNet = StockI[0]['netIncome']
                     Sdate         = StockI[0]["date"]
-                
-                #create a dict for information
+
+                #   create a dict within
                 allstockinfo[stock_symbol] = {}
-                #store information in nested dicts
-                allstockinfo[stock_symbol]['symbol'] = stock_symbol
+                #   create a nested information within a dict
+                allstockinfo[stock_symbol]['symbol'] = stock_symbol 
                 allstockinfo[stock_symbol]['name'] = stock_name
                 allstockinfo[stock_symbol]['industry'] = stock_industry
                 allstockinfo[stock_symbol]['sector'] = stock_sector
@@ -232,11 +241,10 @@ def append_to_file():
                 #with plan 0.2 * number of calls
                 # 0.2* 4 =0.8 = rounded to 1sec
                 # 1 for testing with a data plan and 1728 for full work with a demo plan
-                time.sleep(1728)
+                time.sleep(1)
                 #print(stock_symbol)
                 print("Length of stock symbols",len(total_stock_list))
                 append_json_file(allstockinfo[stock_symbol])
-
     except:
         print('connetion failed')
         pass
@@ -267,13 +275,15 @@ def data_print():
     
     # showing the plot
     fig1.show()
+    #return False
 
 
-while True:
+while do == True:
     localtime = time.ctime(time.time())
     print ("Local current time :", localtime)
     simplefile()
-    checkfile()
-    data_print()
-    break
+    #checkfile()
+    do = checkfile()
+    #data_print()
+    #break
         
